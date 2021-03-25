e97cdc2c5f1390ff23233b097ad0db7e1e80d388
vova <vova.shkar74@gmail.com>
2021-03-25 14:47:23 +0300

    refactoring: add alternative text for multimedia according to rule 1.2 in html-and-css-extended

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="delete btn";
     deleteButtonImg.className="del-img";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='Delete button';
     deleteButton.appendChild(deleteButtonImg);
 
 
